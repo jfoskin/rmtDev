2ce923f04ab32b19674137b61257082d3dcac795
Jazma Foskin <82051355+jfoskin@users.noreply.github.com>
2026-01-08 12:04:38 -0500

    replaced props with an underscore since props will not be getting used typescript does not complain about the way it is sturcted in the function

--- src/components/BookmarksPopover.tsx
+++ src/components/BookmarksPopover.tsx
@@ -2,7 +2,10 @@ import { forwardRef } from "react";
 import { useBookmarkContext } from "../lib/hooks";
 import JobList from "./JobList";
 
-const BookmarksPopover = forwardRef(function (props, ref) {
+// could have passed props as the second parameter in the FOrward ref type of the function but,
+//  if you aren't going to use the props you can just input an _ and typescript won't complain
+
+const BookmarksPopover = forwardRef<HTMLDivElement>(function (_, ref) {
 	const { bookmarkedJobItems, isLoading } = useBookmarkContext();
 	return (
 		<div ref={ref} className="bookmarks-popover">
