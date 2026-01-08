2ceb912cdc80354c471a3dd13b864c7b08248831
Jazma Foskin <82051355+jfoskin@users.noreply.github.com>
2026-01-08 14:58:48 -0500

    created portal for the bookmark popover so that it is not impacted by css

--- src/components/BookmarksPopover.tsx
+++ src/components/BookmarksPopover.tsx
@@ -1,16 +1,18 @@
 import { forwardRef } from "react";
 import { useBookmarkContext } from "../lib/hooks";
 import JobList from "./JobList";
+import { createPortal } from "react-dom";
 
 // could have passed props as the second parameter in the FOrward ref type of the function but,
 //  if you aren't going to use the props you can just input an _ and typescript won't complain
 
 const BookmarksPopover = forwardRef<HTMLDivElement>(function (_, ref) {
 	const { bookmarkedJobItems, isLoading } = useBookmarkContext();
-	return (
+	return createPortal(
 		<div ref={ref} className="bookmarks-popover">
 			<JobList jobItems={bookmarkedJobItems} isLoading={isLoading} />
-		</div>
+		</div>,
+		document.body
 	);
 });
 
