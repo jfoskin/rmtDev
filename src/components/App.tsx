4a1fe97456cd5c9032badcbe20b7abfa4c16f26e
Jazma Foskin <82051355+jfoskin@users.noreply.github.com>
2026-01-10 18:35:51 -0500

    refactoring code so the state that is connected to jobitems can be provider using the contextapi

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -21,56 +21,9 @@ import { PageDirection, SortBy } from "../lib/types";
 function App() {
 	//++++++++++++ State Management Structure +++++++++++\\
 
-	//state
-
-	const { jobItems, isLoading } = useSearchQuery(debouncedSearchText);
-	const [currentPage, setCurrentPage] = useState(1);
-	const [sortBy, setSortBy] = useState<SortBy>("relevant");
-
 	//I could return an array here and rename the item in line [jobItems,IsLoading] = useJobItems(searchText)
 	// even though in my useJobItems custom hook, I returned  jobItemsSliced also,
 	// returning an array you have to be mindful of how you arrange items in the array so they align with the index
-
-	//derived state
-
-	const totalNumberOfResults = jobItems?.length || 0;
-	const totalNumberOfPages = totalNumberOfResults / RESULTS_PER_PAGE;
-
-	const jobItemsSorted =
-		// using the sort item here initially is mutating the array
-		// which is typically a bad thing to do
-		//jobItems?.sort((a, b) => {
-		//instead  im going to spread and return the data in a new array.
-		// this is not the most optimiized way to do this but we also don't want to change the original array
-		[...(jobItems || [])]?.sort((a, b) => {
-			if (sortBy === "relevant") {
-				return b.relevanceScore - a.relevanceScore;
-			} else {
-				return a.daysAgo - b.daysAgo;
-			}
-		});
-
-	const jobItemsSortedAndSliced = jobItemsSorted.slice(
-		currentPage * RESULTS_PER_PAGE - RESULTS_PER_PAGE,
-		currentPage * RESULTS_PER_PAGE
-	);
-
-	//event handler //actions
-
-	const handleChangePage = (direction: PageDirection) => {
-		if (direction === "next") {
-			setCurrentPage((prev) => prev + 1);
-		} else if (direction === "previous") {
-			setCurrentPage((prev) => prev - 1);
-		}
-	};
-
-	const handleChangeSortBy = (newSortBy: SortBy) => {
-		setCurrentPage(1);
-		setSortBy(newSortBy);
-	};
-
-	//++++++++++++ State Management Structure Ends +++++++++++\\
 
 	return (
 		<>
--- src/contexts/JobItemsContextProvider.tsx
+++ src/contexts/JobItemsContextProvider.tsx
@@ -1,15 +1,15 @@
 import { createContext, useState } from "react";
-import { useDebounce } from "../lib/hooks";
+import { useDebounce, useSearchQuery } from "../lib/hooks";
 
-type JobItemContext = {
+type JobItemsContext = {
 	searchText: string;
 	debouncedSearchText: string;
 	handleChangeSearchText: (newSearchText: string) => void;
 };
 
-export const JobItemContext = createContext<JobItemContext | null>(null);
+export const JobItemsContext = createContext<JobItemsContext | null>(null);
 
-function JobItemContextProvider({ children }: { children: React.ReactNode }) {
+function JobItemsContextProvider({ children }: { children: React.ReactNode }) {
 	// the way this is written below is not performant because it runs and re renders
 	//any time. this component is loaded. a better way to optimize is to possibly
 	// use a useEffect and have an empty [] as the second option  instead of passing the variable to useState
@@ -18,24 +18,61 @@ function JobItemContextProvider({ children }: { children: React.ReactNode }) {
 	// 	localStorage.getItem("bookmarkedIds") || ""
 	// );
 
-	const [searchText, setSearchText] = useState("");
-	const debouncedSearchText = useDebounce(searchText, 500);
+	//state
 
-	const handleChangeSearchText = (newSearchText: string) => {
-		setSearchText(newSearchText);
+	const { jobItems, isLoading } = useSearchQuery(debouncedSearchText);
+	const [currentPage, setCurrentPage] = useState(1);
+	const [sortBy, setSortBy] = useState<SortBy>("relevant");
+
+	//derived state
+
+	const jobItemsSorted =
+		// using the sort item here initially is mutating the array
+		// which is typically a bad thing to do
+		//jobItems?.sort((a, b) => {
+		//instead  im going to spread and return the data in a new array.
+		// this is not the most optimiized way to do this but we also don't want to change the original array
+		[...(jobItems || [])]?.sort((a, b) => {
+			if (sortBy === "relevant") {
+				return b.relevanceScore - a.relevanceScore;
+			} else {
+				return a.daysAgo - b.daysAgo;
+			}
+		});
+
+	const jobItemsSortedAndSliced = jobItemsSorted.slice(
+		currentPage * RESULTS_PER_PAGE - RESULTS_PER_PAGE,
+		currentPage * RESULTS_PER_PAGE
+	);
+
+	//event handler //actions
+
+	const handleChangePage = (direction: PageDirection) => {
+		if (direction === "next") {
+			setCurrentPage((prev) => prev + 1);
+		} else if (direction === "previous") {
+			setCurrentPage((prev) => prev - 1);
+		}
 	};
 
+	const handleChangeSortBy = (newSortBy: SortBy) => {
+		setCurrentPage(1);
+		setSortBy(newSortBy);
+	};
+
+	//++++++++++++ State Management Structure Ends +++++++++++\\
+
 	return (
-		<JobItemContext.Provider
+		<JobItemsContext.Provider
 			value={{
-				searchText,
-				debouncedSearchText,
-				handleChangeSearchText,
+				jobItemsSortedAndSliced,
+				handleChangePage,
+				handleChangeSortBy,
 			}}
 		>
 			{children}
-		</JobItemContext.Provider>
+		</JobItemsContext.Provider>
 	);
 }
 
-export default JobItemContextProvider;
+export default JobItemsContextProvider;
